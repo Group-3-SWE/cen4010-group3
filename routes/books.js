55ce8630ae9af74f3b8ba90fe325d2b365171853
Godmiliut <supermiliut72@gmail.com>
2023-09-29 02:48:30 -0400

    Added dynamic route for GET request to filter books by genre

--- routes/books.js
+++ routes/books.js
@@ -6,6 +6,17 @@ router.get("/", (req, res) => {
     res.send("This is the books route!");
 })
 
+router.get("/genre/:genre", async (req, res, next) =>{
+    try {
+        // get the list of books belonging to a specific genre
+        const { genre } = req.params
+        const listOfBooks = await Books.findAll({ where: {BGenre: genre} })
+        return res.status(200).json({ listOfBooks })
+    } catch(err){
+        next(err)
+    }
+})
+
 router.post("/", async (req, res) => {
     const book = req.body;
     await Books.create(book);
