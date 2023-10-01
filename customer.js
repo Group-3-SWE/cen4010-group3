5b66e9caf0943a690a719a3a9c90dd6d2479f11b
williambjr <wbosc001@fiu.edu>
2023-10-01 19:50:02 +0100

    Hello World Code

--- customer.js
+++ customer.js
@@ -3,3 +3,12 @@ const express = require('express');
 // Express Initialize
 const app = express();
 
+const port = 3001
+
+app.listen(port,()=> {
+    console.log('listen port 3001');
+})
+
+app.get('/hello_world', (req,res)=>{
+    res.send('Hello World');
+})
